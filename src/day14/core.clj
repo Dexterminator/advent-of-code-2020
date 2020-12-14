5eecf0be0aa0d0aa1bb016ae0ddf58038638c81f
Dexter Gramfors <dexter.gramfors@trioptima.com>
2020-12-14 17:00:22 -0500

    Solved day 14 part 2

--- src/day14/core.clj
+++ src/day14/core.clj
@@ -1,36 +1,69 @@
 (ns day14.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :refer [subsets]]
+            [medley.core :refer [indexed]]))
 
 (def input (slurp "src/day14/input.txt"))
-;(def input "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0")
 
-(defn get-mask [line]
-  (->> line
-       (re-find #"\S{36}")
+;; Common
+(defn apply-ops [ops n]
+  (reduce (fn [n [i bit-fn]] (bit-fn n i)) n ops))
+
+(defn parse-mask-line [line]
+  (re-find #"\S{36}" line))
+
+(defn parse-mem-line [line]
+  (let [[_ addr n] (re-matches #"mem\[(\d+)\] = (\d+)" line)]
+    [(Long. addr) (Long. n)]))
+
+(defn solve-fn [get-mask-fn write-value-fn]
+  (fn []
+    (->> input
+         (str/split-lines)
+         (reduce (fn [{:keys [mask] :as state} line]
+                   (if-let [mask-line (parse-mask-line line)]
+                     (assoc state :mask (get-mask-fn mask-line))
+                     (update state :mem write-value-fn mask (parse-mem-line line))))
+                 {:mask [] :mem {}})
+         :mem
+         (map val)
+         (apply +))))
+
+;; Part 1
+(def c->op {\0 bit-clear \1 bit-set})
+(defn get-ops [mask-str]
+  (->> mask-str
        (reverse)
-       (map-indexed (fn [i c]
-                      (case c
-                        \0 [i bit-clear]
-                        \1 [i bit-set]
-                        nil)))
+       (map-indexed (fn [i c] (when (contains? c->op c) [i (c->op c)])))
        (remove nil?)))
 
-(defn write-value [mem line mask]
-  (let [[_ addr n] (re-matches #"mem\[(\d+)\] = (\d+)" line)]
-    (assoc mem addr (reduce (fn [n [i bit-fn]] (bit-fn n i)) (Integer. n) mask))))
+(defn write-value [mem mask [addr n]]
+  (assoc mem addr (apply-ops mask n)))
 
-(defn part1 []
-  (->> input
-       (str/split-lines)
-       (reduce (fn [{:keys [mask] :as state} line]
-                 (if (str/starts-with? line "ma")
-                   (assoc state :mask (get-mask line))
-                   (update state :mem write-value line mask)))
-               {:mask []
-                :mem  {}})
-       :mem
-       (map val)
-       (apply +)))
+(def part1 (solve-fn get-ops write-value))
 
+;; Part 2
+(defn index-subset->ops [indices subset]
+  (map #(if (contains? subset %) [% bit-set] [% bit-clear])
+       indices))
+
+(defn get-ops-2 [mask-str]
+  (let [groups (->> mask-str (reverse) (indexed) (group-by second))
+        fixed-indices (get groups \1)
+        floating-indices (map first (get groups \X))]
+    {:fixed-ops    (map (fn [[i _]] [i bit-set]) fixed-indices)
+     :floating-ops (->> floating-indices (subsets) (map set) (map (partial index-subset->ops floating-indices)))}))
+
+(defn get-addrs [floating-ops fixed-ops addr]
+  (map (fn [ops] (apply-ops fixed-ops (apply-ops ops addr))) floating-ops))
+
+(defn write-value-2 [mem {:keys [floating-ops fixed-ops]} [addr n]]
+  (reduce #(assoc %1 %2 n)
+          mem
+          (get-addrs floating-ops fixed-ops addr)))
+
+(def part2 (solve-fn get-ops-2 write-value-2))
+
+;; Answers
 (println (part1))
-
+(println (part2))
