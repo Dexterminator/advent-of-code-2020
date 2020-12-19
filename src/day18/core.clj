404071258f4a6ef3891bfe313cba6acb1ca518e1
Dexter Gramfors <dexter.gramfors@trioptima.com>
2020-12-19 02:35:12 -0500

    solved day 19 part 2

--- src/day18/core.clj
+++ src/day18/core.clj
@@ -3,64 +3,50 @@
             [util :refer [parse-long]]
             [clojure.string :as str]))
 
-(def input "1 + 2 * 3 + 4 * 5 + 6")
-;(def input "1 + (2 * 3) + (4 * (5 + 6))")
-;(def input "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
-(def input "2 * 3 + (4 * 5)")
-(def input "5 + (8 * 3 + 9 + 3 * 4 * 3)")
-(def input "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
-(def input "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
 (def input (slurp "src/day18/input.txt"))
 
-;2 * 3 + (4 * 5)
-(* 2 (+ 3 (* 4 5)))
+(def base
+  "<string> = expr+
+   <nested-expr> = <left> expr <right>
+   left = '('
+   right = ')'
+   <ws> = #'[ \\t]+'
+   mul = '*'
+   add = '+'
+   num = #'\\d+'")
 
-(def parser
+(def parser1
   (insta/parser
-    "
-    <string> = expr+
-    expr = val <ws> op <ws> val
-    <val> = expr | nested-expr | num
-    <nested-expr> = <left> expr <right>
-    left = '('
-    right = ')'
-    <ws> = #'[ \\t]+'
-    num = #'\\d+'
-    op = '*' | '+'
-    "))
+    (str base
+         "expr = val <ws> op <ws> val
+         <val> = expr | nested-expr | num
+         <op> = add | mul")))
 
 (def parser2
   (insta/parser
-    "
-    <string> = expr+
-    <expr> = mul-expr | nested-expr | add-expr
-    add-expr = add-val <ws> add <ws> add-val
-    mul-expr = mul-val <ws> mul <ws> mul-val
-    add = '+'
-    mul = '*'
-    <mul-val> = nested-expr | expr | num
-    <add-val> = add-expr | nested-expr | num
-    <nested-expr> = <left> expr <right>
-    left = '('
-    right = ')'
-    <ws> = #'[ \\t]+'
-    num = #'\\d+'
-    "))
+    (str base
+         "<expr> = add-expr | mul-expr
+         add-expr = add-val <ws> add <ws> add-val
+         mul-expr = mul-val <ws> mul <ws> mul-val
+         <mul-val> = nested-expr | expr | num
+         <add-val> = add-expr | nested-expr | num")))
+
+(def lispify (comp (fn [[x y z]] (list y x z)) list))
 
 (def transformations {:num      parse-long
-                      :op       symbol
                       :add      symbol
                       :mul      symbol
-                      :add-expr (comp (fn [[x y z]] (list y x z)) list)
-                      :mul-expr (comp (fn [[x y z]] (list y x z)) list)
-                      :expr     (comp (fn [[x y z]] (list y x z)) list)})
+                      :add-expr lispify
+                      :mul-expr lispify
+                      :expr     lispify})
 
-(defn part1 []
+(defn solve [parser]
   (->> input
        (str/split-lines)
-       (map parser2)
+       (map parser)
        (map #(first (insta/transform transformations %)))
        (map eval)
        (apply +)))
 
-(println #p (part1))
+(println (solve parser1))
+(println (solve parser2))
