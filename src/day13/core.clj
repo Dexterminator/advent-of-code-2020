81f27bc4c6baf5c2d2bcf3b90ecb89ea1fd1a60c
Dexter Gramfors <dexter.gramfors@trioptima.com>
2020-12-19 14:26:10 -0500

    Solve day 13 part 2

--- src/day13/core.clj
+++ src/day13/core.clj
@@ -1,37 +1,50 @@
 (ns day13.core
-  (:require [medley.core :refer [indexed find-first]]))
+  (:require [medley.core :refer [indexed]]
+            [util :refer [parse-long]]))
 
-;(def input (slurp "src/day13/input.txt"))
-;(def input "939\n7,13,x,x,59,x,31,19")
-(def input "939\n17,x,13,19")
-;(def input "939\n67,7,59,61")
-;(def input "939\n67,x,7,59,61")
-;(def input "939\n67,7,x,59,61")
-;(def input "939\n1789,37,47,1889")
-(def nums (->> input (re-seq #"\d+") (map #(Integer. %))))
+(def input (slurp "src/day13/input.txt"))
 
 (defn part1 []
-  (let [[ts & ids] nums]
+  (let [[ts & ids] (->> input (re-seq #"\d+") (map parse-long))]
     (->> ids
          (map (fn [id] [id (- id (rem ts id))]))
          (apply min-key second)
          (apply *))))
 
-(println (part1))
+(defn abs [n]
+  (if (neg? n) (- n) n))
 
-(defn ts-seq [ts]
-  (map #(*' ts %) (range)))
+(defn extended-gcd [a b]
+  (cond (zero? a) [(abs b) 0 1]
+        (zero? b) [(abs a) 1 0]
+        :else (loop [s 0 s0 1 t 1 t0 0 r (abs b) r0 (abs a)]
+                (if (zero? r)
+                  [r0 s0 t0]
+                  (let [q (quot r0 r)]
+                    (recur (- s0 (* q s)) s
+                           (- t0 (* q t)) t
+                           (- r0 (* q r)) r))))))
+
+(defn solve [n a]
+  (let [prod (apply * n)
+        reducer (fn [sum [ni ai]]
+                  (let [p (quot prod ni)
+                        [_ inv-p] (extended-gcd p ni)]
+                    (+ sum (* ai inv-p p))))
+        sum-prod (reduce reducer 0 (map vector n a))]
+    (mod sum-prod prod)))
 
 (defn part2 []
-  (let [[first-ts & ids] (->> input
-                              (re-seq #"\d+|x")
-                              (rest)
-                              (indexed)
-                              (remove #(= "x" (second %)))
-                              (map (fn [[i n]] [i (Integer. n)]))
-                              (sort-by second >))]
-    (find-first
-      #(every? (fn [[i n]] (= i (- n (mod % n)))) ids)
-      (ts-seq first-ts))))
+  (let [ids (->> input
+                 (re-seq #"\d+|x")
+                 (rest)
+                 (indexed)
+                 (remove #(= "x" (second %)))
+                 (map (fn [[i n]]
+                        (let [n (parse-long n)]
+                          [n (- n i)]))))
+        [n a] (apply map vector ids)]
+    (solve n a)))
 
+(println (part1))
 (println (part2))
